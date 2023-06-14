ea61b1457af09a9338e4c36eda2b58dd0e22e639
Md Masud karim <msmasud578@gmail.com>
2023-06-15 01:22:32 +0600

    initial commit

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <argparse.h>
+#include <halstead/test.h>
+
+int main(int argc, char** argv) {
+	printf("Welcome to the halstead metrics analyzer ... \n");
+	greet();
+	return 0;
+}
---
+++ src/test.c
@@ -0,0 +1,6 @@
+#include <halstead/test.h>
+#include <argparse.h>
+#include <stdio.h>
+void greet(void) {
+	printf ("Hello world this is the greet file\n");
+}
