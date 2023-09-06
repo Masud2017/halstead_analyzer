50a6b6ebf1c0355dff959d921f0992c2f4feec03
Md Masud karim <msmasud578@gmail.com>
2023-09-06 23:24:55 +0600

    resolved the gtest error issue

---
+++ build/CMakeFiles/halstead_analyzer.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/e/intellijproject/c_cpp_codes/halstead_matrics_analyzer/src/ast_parser.c" "CMakeFiles/halstead_analyzer.dir/src/ast_parser.c.o" "gcc" "CMakeFiles/halstead_analyzer.dir/src/ast_parser.c.o.d"
+  "/mnt/e/intellijproject/c_cpp_codes/halstead_matrics_analyzer/src/metrics.c" "CMakeFiles/halstead_analyzer.dir/src/metrics.c.o" "gcc" "CMakeFiles/halstead_analyzer.dir/src/metrics.c.o.d"
+  "/mnt/e/intellijproject/c_cpp_codes/halstead_matrics_analyzer/src/test.c" "CMakeFiles/halstead_analyzer.dir/src/test.c.o" "gcc" "CMakeFiles/halstead_analyzer.dir/src/test.c.o.d"
+  "/mnt/e/intellijproject/c_cpp_codes/halstead_matrics_analyzer/main.cc" "CMakeFiles/halstead_analyzer.dir/main.cc.o" "gcc" "CMakeFiles/halstead_analyzer.dir/main.cc.o.d"
+  "/mnt/e/intellijproject/c_cpp_codes/halstead_matrics_analyzer/test/TestParser.cc" "CMakeFiles/halstead_analyzer.dir/test/TestParser.cc.o" "gcc" "CMakeFiles/halstead_analyzer.dir/test/TestParser.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/mnt/e/intellijproject/c_cpp_codes/halstead_matrics_analyzer/build/lib/spdlog/CMakeFiles/spdlog.dir/DependInfo.cmake"
+  "/mnt/e/intellijproject/c_cpp_codes/halstead_matrics_analyzer/build/lib/argparser/CMakeFiles/argparse.dir/DependInfo.cmake"
+  "/mnt/e/intellijproject/c_cpp_codes/halstead_matrics_analyzer/build/_deps/googletest-build/googletest/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  "/mnt/e/intellijproject/c_cpp_codes/halstead_matrics_analyzer/build/_deps/googletest-build/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ main.cc
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <argparse.h>
+#include <halstead/halstead.h>
+
+int main(int argc, char** argv) {
+	printf("Welcome to the halstead metrics analyzer ... \n");
+//	if (argc > 1) {
+//		Operator* operator = get_operator_list(argv[1]);
+//	} else {
+//		perror("You have to provide the filename \n");
+//	}
+	return 0;
+}
