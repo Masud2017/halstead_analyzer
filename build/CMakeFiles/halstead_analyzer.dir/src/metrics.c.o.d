8cdfc75ee8065564758c11308fdba9e60baa4c3d
Md Masud karim <msmasud578@gmail.com>
2023-09-06 23:09:45 +0600

    removed the binary

--- build/CMakeFiles/halstead_analyzer.dir/src/metrics.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/halstead_analyzer.dir/src/metrics.c.o: \
- /mnt/e/intellijproject/c_cpp_codes/halstead_matrics_analyzer/src/metrics.c \
- /usr/include/stdc-predef.h \
- /mnt/e/intellijproject/c_cpp_codes/halstead_matrics_analyzer/include/halstead/metrics.h
--- build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/e/intellijproject/c_cpp_codes/halstead_matrics_analyzer/build/_deps/googletest-src/googletest
-# Build directory: /mnt/e/intellijproject/c_cpp_codes/halstead_matrics_analyzer/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
